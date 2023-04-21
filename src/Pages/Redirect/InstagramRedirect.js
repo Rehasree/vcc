887361894e50e2e6865e498795b5f2f686d84ee9
Rehasree <rehasreekoneru@gmail.com>
2023-04-22 03:11:09 +0530

    added code

--- src/Pages/Redirect/InstagramRedirect.js
+++ src/Pages/Redirect/InstagramRedirect.js
@@ -1,6 +1,38 @@
 import React from 'react'
-
+import { useEffect } from 'react';
+import axios from 'axios'
 const InstagramRedirect = () => {
+    const [accessToken, setAccessToken] = React.useState(null);
+
+    useEffect(() => {
+        console.log('URL:', window.location.href);
+        const urlParams = new URLSearchParams(window.location.search);
+        const code = urlParams.get('code');
+        console.log('code', code);
+    
+        if (code) {
+          const exchangeCodeForToken = async () => {
+            const redirectUri = 'https://main--chipper-pie-1ad82e.netlify.app/instagram-redirect/';
+            const appId = '1422036221904968';
+            const appSecret = '438aa50628f6488b5cd05fce3992b6b1';
+    
+            try {
+              const response = await axios.post('https://activeplushvolcano.rehasreekoneru.repl.co/api/exchange-code', {
+                code,
+                appId,
+                appSecret,
+                redirectUri,
+              });
+              console.log('response');
+              setAccessToken(response.data.access_token);
+            } catch (error) {
+              console.error('Error exchanging code for token:', error);
+            }
+          };
+    
+          exchangeCodeForToken();
+        }
+      }, []);
   return (
     <div>InstagramRedirect</div>
   )
